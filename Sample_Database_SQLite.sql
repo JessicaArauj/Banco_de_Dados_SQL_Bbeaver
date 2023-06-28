0e9ca99b1a9d3e39cf31ece4853d1cb71a92252c
Jessica Teixeira Araujo <111321791+JessicaTeixeiraAraujo@users.noreply.github.com>
2023-06-28 10:21:57 -0300

    feat: script created query database

--- Sample_Database_SQLite.sql
+++ Sample_Database_SQLite.sql
@@ -109,3 +109,122 @@ WHERE Title LIKE "%DISC%"
 SELECT * 
 FROM Artist
 WHERE Name LIKE "AC_DC" -- _ é um símbolo coringa
+
+-- Manipulação de dados
+
+SELECT *
+FROM Genre
+LIMIT 10
+
+SELECT *
+FROM Genre
+WHERE Name like "MPB"
+
+INSERT INTO Genre (GenreId, Name)
+VALUES (NULL, "MPB")
+
+-- ID 26
+
+SELECT *
+FROM Artist
+WHERE Name like "TOQUINHO"
+
+INSERT INTO Artist (Name)
+VALUES ("Toquinho")
+
+-- 276
+
+SELECT *
+FROM Track
+
+INSERT INTO Track
+VALUES (NULL, "Aquarela", NULL, 1, 26, "Toquinho e Vinicius", 2500000, 8000000, 5.99)
+
+SELECT *
+FROM Track
+WHERE GenreId = 26
+
+-- Atualização de dados de uma tabela
+
+SELECT *
+FROM Customer
+WHERE CustomerId = 2
+
+UPDATE Customer
+SET Company = "EBAC"
+WHERE CustomerId = 2
+
+SELECT *
+FROM Customer
+WHERE Country like "Bra_il"
+
+UPDATE Customer
+SET Country = "Brasil"
+WHERE Country like "Bra_il"
+
+-- Exclusão de registro -- Delete
+
+SELECT *
+FROM Invoice
+WHERE Total >= 20
+	AND BillingCountry like "USA"
+	
+DELETE
+FROM Invoice
+WHERE Total >= 20
+	AND BillingCountry like "USA"
+	
+
+-- Junção de dados
+	
+SELECT COUNT(*)
+FROM Album
+
+SELECT COUNT(*)
+FROM Artist
+
+-- forma errada de juntar base de dados
+SELECT COUNT(*)
+FROM Album, Artist
+
+-- Join
+
+SELECT
+	Album.Title,
+	Artist.Name
+FROM Album
+INNER JOIN Artist
+	ON Album.AlbumId = Artist.ArtistId
+WHERE name like "Iron Maiden"
+
+SELECT
+	Album.Title as "Título do album",
+	Artist.Name as "Nome do artista",
+	Track.Name as "Nome da música"
+FROM Album
+INNER JOIN Artist
+	ON Album.AlbumId = Artist.ArtistId
+INNER JOIN Track
+	ON Album.AlbumId = Track.AlbumId
+
+-- Criação de tabelas
+	
+	CREATE TABLE Aluno(
+		AlunosId INTEGER PRIMARY KEY AUTOINCREMENT,
+		Nome VARCHAR (30),
+		Matricula INTEGER (5)
+);
+
+SELECT * FROM Aluno
+
+INSERT INTO Aluno (Nome, Matricula)
+VALUES
+	("Fábio Araújo", 12345),
+	("Mariana Silva", 43566),
+	("José Ernesto", 54321)
+	
+-- Excluindo tabela
+	
+DROP TABLE Aluno
+
+
